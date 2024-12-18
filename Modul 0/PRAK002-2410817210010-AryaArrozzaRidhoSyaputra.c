8201ce79ef9a69085113a5c00b855f36adac20d0
Arya Arrozza Ridho Syaputra <ozzaboy123@gmail.com>
2024-12-18 22:42:47 +0800

    Delete Modul 0 directory

--- Modul 0/PRAK002-2410817210010-AryaArrozzaRidhoSyaputra.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("Selamat Pagi, Arya Arrozza Ridho Syaputra\n");
-    printf("Selamat Siang, Arya Arrozza Ridho Syaputra\n");
-    printf("Selamat Malam, Arya Arrozza Ridho Syaputra") ;
-    return 0;
-}
--- Modul 0/PRAK002-2410817210010-AryaArrozzaRidhoSyaputra.py
+++
@@ -1,3 +0,0 @@
-print("Selamat Pagi, Arya Arrozza Ridho Syaputra")
-print("Selamat Siang, Arya Arrozza Ridho Syaputra")
-print("Selamat Malam, Arya Arrozza Ridho Syaputra")
